a958078ca6739088aee8e7c453a48b87eab6bdf7
LENOVO <LENOVO@LAPTOP-6T21J694>
2026-07-02 02:39:28 -0700

    project 2 coba coba Main.java

---
+++ src/main/java/com/mycompany/projectcobacoba/Projectcobacoba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectcobacoba;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Projectcobacoba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello sintiaa");
+    }
+}
